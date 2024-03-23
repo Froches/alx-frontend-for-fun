8a2047f3bc5388e4d25587b69f72ac64b036a9ae
Favour Michael <michaelfavour418@gmail.com>
2024-03-23 11:08:23 +0100

    markdowntohtml

---
+++ markdown2html.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+import os
+
+if len(sys.argv) < 3:
+    sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+    sys.exit(1)
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+if not os.path.exists(input_file):
+    sys.stderr.write(f"Missing {input_file}\n")
+    sys.exit(1)
+
+sys.exit(0)
+
+# if __name__ == "__main__":
