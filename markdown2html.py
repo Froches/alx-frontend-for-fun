f812b1bbde78d14c40e3fea514f3c0eb83f59f9e
Favour Michael <michaelfavour418@gmail.com>
2024-03-25 05:20:40 +0100

    markdown to HTML

--- markdown2html.py
+++ markdown2html.py
@@ -2,20 +2,33 @@
 """
 Creating a program that converts Markdown files to html files
 """
+
 import sys
 import os
+import markdown
 
-if len(sys.argv) < 3:
-    sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
-    sys.exit(1)
+def convert_markdown_to_html(input_file, output_file):
+    try:
+        with open(input_file, 'r') as md_file:
+            markdown_text = md_file.read()
+            html_content = markdown.markdown(markdown_text)
+            with open(output_file, 'w') as html_file:
+                html_file.write(html_content)
+    except FileNotFoundError:
+        print(f"Missing {input_file}", file=sys.stderr)
+        sys.exit(1)
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py <input_file> <output_file>", file=sys.stderr)
+        sys.exit(1)
+    
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
 
-if not os.path.exists(input_file):
-    sys.stderr.write(f"Missing {input_file}\n")
-    sys.exit(1)
-
-sys.exit(0)
-
-# if __name__ == "__main__":
+    if not os.path.exists(input_file):
+        print(f"Missing {input_file}", file=sys.stderr)
+        sys.exit(1)
+    
+    convert_markdown_to_html(input_file, output_file)
+    sys.exit(0)
