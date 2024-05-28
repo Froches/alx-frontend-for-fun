5d06dfb62ee563e310be68374aeb4e9b68dd538e
Favour Michael <michaelfavour418@gmail.com>
2024-05-28 09:18:41 +0100

    markdown to html

--- markdown2html.py
+++ markdown2html.py
@@ -1,57 +1,25 @@
 #!/usr/bin/python3
-"""
-Creating a program that converts Markdown files to html files
-"""
+"""markdown2html.py - Converts a markdown file to HTML"""
 
 
-import sys
-import os
 import markdown
+import sys
 
 
-def parse_markdown_headings(markdown_text):
-    """
-    Parses markdown headings
-    """
-    html_content = ""
-    new_lines = markdown_text.split("\n")
-    for line in new_lines:
-        if line.startswith("#"):
-            h_level = line.count("#")
-            title = line.strip("#").strip()
-            html_content += f"<h{h_level}>{title}</h{h_level}>\n"
-        else:
-            html_content += f"{line}\n"
-    return html_content
+if len(sys.argv) != 3:
+    print("Usage: ./markdown2html.py README.md README.html")
+    sys.exit(1)
 
+markdown_file = sys.argv[1]
+html_file = sys.argv[2]
 
-def convert_markdown_to_html(input_file, output_file):
-    """
-    Does the conversion
-    """
-    try:
-        with open(input_file, 'r') as md_file:
-            markdown_text = md_file.read()
-            html_content = markdown.markdown(markdown_text)
-            with open(output_file, 'w') as html_file:
-                html_file.write(html_content)
-    except FileNotFoundError:
-        print(f"Missing {input_file}", file=sys.stderr)
-        sys.exit(1)
+try:
+    with open(markdown_file, 'r') as file:
+        text = file.read()
+        html = markdown.markdown(text)
+except FileNotFoundError:
+    print("Missing {}".format(markdown_file))
+    sys.exit(1)
 
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py <input_file> <output_file>",
-              file=sys.stderr)
-        sys.exit(1)
-
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
-
-    if not os.path.exists(input_file):
-        print(f"Missing {input_file}", file=sys.stderr)
-        sys.exit(1)
-
-    convert_markdown_to_html(input_file, output_file)
-    sys.exit(0)
+with open(html_file, 'w') as file:
+    file.write(html)
